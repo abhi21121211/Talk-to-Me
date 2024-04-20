009469b453507bbe8f0ed1523564464aaaf06e73
abhi21121211 <111838547+abhi21121211@users.noreply.github.com>
2024-04-20 14:28:14 +0530

    Mongodb connected

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const connectDB = require("./config/db");
+const colors = require('colors');
 // const data = require('/data/data.js');
 
-const app = express();
+
 dotenv.config();
+connectDB();
+const app = express();
 
 // console.log(data)
 app.get('/', (req, res) => {
@@ -14,8 +18,9 @@ app.get('/chats', (req, res) => {
     // res.send(data);
    
 
+
 })
 
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`.yellow.bold));
--- frontend/src/Pages/HomePage.js
+++ frontend/src/Pages/HomePage.js
@@ -1,10 +1,47 @@
 import React from 'react'
-
+import { Box, Container, Tab, TabList, TabPanel, TabPanels, Tabs, Text } from '@chakra-ui/react';
+import { color } from 'framer-motion';
+import Login from '../components/Authentication/Login';
+import Signup from '../components/Authentication/Signup';
 function HomePage() {
   return (
-    <div>
-      HomePage
-    </div>
+    <Container maxW="xl" centerContent >
+      <Box
+        d='flex'
+        justifyContent='center'
+
+
+        p={3}
+        bg={"white"}
+        w="100%"
+        m="40px 0 15px 0"
+        borderRadius="lg"
+        borderWidth="1px"
+
+
+      >
+        <Text fontSize="4xl"
+          fontFamily="Sniglet" color="black"
+          textAlign={"center"}
+        >Talk to Me</Text>
+      </Box>
+      <Box bg="white" w="100%" p={4} color="black" borderRadius="lg" borderWidth="1px">
+        <Tabs variant='soft-rounded' >
+          <TabList md="1em">
+            <Tab width={"50%"}>Login</Tab>
+            <Tab w={"50%"}>Signup</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              <Login/>
+            </TabPanel>
+            <TabPanel>
+              <Signup/>
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Box>
+    </Container>
   )
 }
 
