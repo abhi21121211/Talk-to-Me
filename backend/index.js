8c6ad275de7937f855827ba6c61b0aa60d8ee9f9
abhi21121211 <111838547+abhi21121211@users.noreply.github.com>
2024-04-21 12:28:42 +0530

    group chat done

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require("./config/db");
 const userRoutes = require('./routes/userRoutes');
+const chatRoutes = require('./routes/chatRoutes');
 const { notFound , errorHandler} = require('./middleware/errorMiddleware');
 
 const colors = require('colors');
@@ -19,6 +20,7 @@ app.get('/', (req, res) => {
 });
 
 app.use('/api/user', userRoutes);
+app.use('/api/chat', chatRoutes);
 app.use(notFound)
 app.use(errorHandler);
 
--- backend/models/chatModel.js
+++ backend/models/chatModel.js
@@ -1,26 +1,17 @@
 const mongoose = require("mongoose");
+
 const chatModel = mongoose.Schema(
   {
-    chaName: { type: string, trim: true },
+    chatName: { type: String, trim: true },
     isGroupChat: { type: Boolean, default: false },
-    users: [
-      {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
-      },
-    ],
+    users: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
     latestMessage: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "Message",
     },
-    groupAdmin: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-    },
+    groupAdmin: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
   },
-  {
-    timestamp: true,
-  }
+  { timestamps: true }
 );
 
 const Chat = mongoose.model("Chat", chatModel);
